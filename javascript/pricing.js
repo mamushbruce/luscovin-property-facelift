7c8b97225d392ef127b8c6927fa6330d920e12ef
mamushbruce <156106657+mamushbruce@users.noreply.github.com>
2024-02-15 19:21:31 +0300

    Add files via upload

---
+++ javascript/pricing.js
@@ -0,0 +1,19 @@
+function showContacts() {
+    // Add your logic to display contacts menu
+  }
+  
+  function showServiceForm() {
+    var serviceForm = document.getElementById('service-form');
+    serviceForm.classList.remove('hidden');
+  }
+  
+  function clearForm() {
+    var serviceForm = document.getElementById('service-form');
+    serviceForm.classList.add('hidden');
+    // Add logic to clear form fields
+  }
+  
+  function agreeToTerms() {
+    // Add your logic for agreeing to terms
+  }
+  
