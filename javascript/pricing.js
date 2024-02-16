8f68ddd5271f4384581b8ebcc1872ce44d388ab6
mamushbruce <156106657+mamushbruce@users.noreply.github.com>
2024-02-16 19:28:04 +0300

    Add files via upload

--- javascript/pricing.js
+++ javascript/pricing.js
@@ -16,4 +16,53 @@ function showContacts() {
   function agreeToTerms() {
     // Add your logic for agreeing to terms
   }
+
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+//for the dropdown section
+const price = getElement('.pricing-down')
+const pricingBtnDOM = getElement('.pricing-btn')
+
+
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+})
+
+pricingBtnDOM.addEventListener('click', () => {
+  price.classList.toggle('show-pricing')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
+
+
+
+// const getElement = (selector) => {
+//   const elements = document.querySelector(selector)
+
+//   if (elements) return elements
+//   throw Error(
+//     `Please double check your class names, there is no ${selector} class`
+//   )
+// }
+// //for the dropdown section
+// const price = getElement('.pricing-down')
+// const pricingBtnDOM = getElement('.pricing-btn')
+
+// pricingBtnDOM.addEventListener('click', () => {
+//   price.classList.toggle('show-pricing')
+// })
+
+
+
   
